9814d52f6b7ada75411803443f90bd290923349c
Murilo Besson <murilo.besson@outlook.com>
2026-09-01 20:10:44 -0300

    chore: initialize ticket API

---
+++ src/test/java/com/api/suporte/SuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.suporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
