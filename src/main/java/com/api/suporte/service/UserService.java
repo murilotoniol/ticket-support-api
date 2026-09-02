a01f7701c1abf394b43d2dc16f17ab6b1e37f292
Murilo Besson <murilo.besson@outlook.com>
2026-09-02 19:38:55 -0300

    feat: create addUser method in UserService

--- src/main/java/com/api/suporte/service/UserService.java
+++ src/main/java/com/api/suporte/service/UserService.java
@@ -1,6 +1,8 @@
 package com.api.suporte.service;
 
 import com.api.suporte.domain.User;
+import com.api.suporte.dto.UserRequestDTO;
+import com.api.suporte.dto.UserResponseDTO;
 import com.api.suporte.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,5 +21,18 @@ public class UserService {
     @Transactional(readOnly = true)
     public List<User> getAll(){
         return userRepository.findAll();
+    }
+
+    @Transactional
+    public UserResponseDTO addUser(UserRequestDTO dto){
+        User user = new User(dto.name(), dto.email());
+
+//        if(userRepository.existsByEmail(dto.email()){
+//            throw new EmailAlreadyExistsException("Email já cadastrado!");
+//        }
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDTO(savedUser);
     }
 }
