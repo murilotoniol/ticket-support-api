ab254c9cf930e9a8babf0dce465cbd0ac3db790e
Murilo Besson <murilo.besson@outlook.com>
2026-09-02 20:12:23 -0300

    feat: create TicketRepository

---
+++ src/main/java/com/api/suporte/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.api.suporte.repository;
+
+import com.api.suporte.domain.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+}
